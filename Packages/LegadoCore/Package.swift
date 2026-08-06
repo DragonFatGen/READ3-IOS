e7300f5c4b0745fead72b4a4a825158e4ce0441b
xzy <chcz_xzy@163.com>
2026-08-06 15:05:51 +0800

    fix: declare LegadoCore Apple deployment targets

--- Packages/LegadoCore/Package.swift
+++ Packages/LegadoCore/Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "LegadoCore",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "LegadoCore",
