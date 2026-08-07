6c10858010fdbc471a360e7dbb60cf8bd322d668
xzy <chcz_xzy@163.com>
2026-08-07 14:36:17 +0800

    11

---
+++ codex-proxy.ps1
@@ -0,0 +1,8 @@
+$env:HTTP_PROXY="http://127.0.0.1:3067"
+$env:HTTPS_PROXY="http://127.0.0.1:3067"
+$env:http_proxy="http://127.0.0.1:3067"
+$env:https_proxy="http://127.0.0.1:3067"
+$env:NO_PROXY="localhost,127.0.0.1,::1"
+$env:no_proxy="localhost,127.0.0.1,::1"
+
+codex
